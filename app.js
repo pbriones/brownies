3566ad83ac99c877196a7ac341b1ed8068b91ac1
Phil Briones <Phil Briones>
2016-11-30 19:12:55 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+app.listen(port, ip);
+console.log('Server running on http://%s:%s', ip, port);
