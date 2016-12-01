5b9a0f256fd61298b6b6d9af4ad63e77dbb48225
Phil Briones <Phil Briones>
2016-11-30 19:31:42 -0500

    add index.html

--- app.js
+++ app.js
@@ -4,5 +4,12 @@ var app = express();
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
+app.get('/', function (req, res) {
+    res.sendFile('index.html', {
+        root: './view'
+    });
+});
+
+
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
