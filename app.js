202976f1b723e17fde5ea213175d5dd35b80e530
Phil Briones <PhilB@SurfaceBrownies.(none)>
2016-12-17 14:52:26 -0500

    add yelp dependency

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
-var express = require('express');
-var app = express();
+'use strict'
+const dotenv = require('dotenv');
+const express = require('express');
+const app = express();
+dotenv.load();
+const CLIENT_ID = process.env.YELP_ID;
+const CLIENT_SECRET = process.env.YELP_SECRET;
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
-
+let port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+let ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 app.get('/', function (req, res) {
-    res.sendFile('index.html', {
-        root: './view'
-    });
+  res.sendFile('index.html', {
+    root: './view'
+  });
 });
 
-
+var Yelp = require('yelp-fusion-v3');
+let yelp = new Yelp({
+  client_id: CLIENT_ID,
+  client_secret: CLIENT_SECRET
+});
+yelp.getBusinesses({ location: '45150' })
+  .then(console.log)
+  .catch(console.error);
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
