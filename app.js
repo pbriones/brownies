463ca73beee8fecda317b7c8276920b1135d1904
Phil Briones <PhilB@SurfaceBrownies.(none)>
2017-03-10 07:28:16 -0500

    update port and ip

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 const route = require('./route');
 
-let port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
-let ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+let port = process.env.PORT || 8080;
+let ip = process.env.IP || '0.0.0.0';
 app.use('/', route);
 
-app.listen(port, ip, () => {
+app.listen(port, () => {
   console.log(`Running on: http://${ip}:${port}`);
   console.log(`Started at: ${new Date().toISOString()}`);
 });
