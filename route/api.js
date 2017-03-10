92d28acbefc924cb24a771c3900f5f1f4d08f743
Phil Briones <PhilB@SurfaceBrownies.(none)>
2017-03-10 07:12:13 -0500

    update api route to return ok on get

--- route/api.js
+++ route/api.js
@@ -5,6 +5,7 @@ const googlemaps = require('./googlemaps');
 const restaurant = require('./restaurant');
 let router = express.Router();
 
+router.get('/', (req, res) => res.send('ok'));
 router.use('/yelp', yelp);
 router.use('/googlemaps', googlemaps);
 router.use('/restaurant', restaurant);
